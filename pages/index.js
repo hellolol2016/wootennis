c94582667fe2c69dcf80ddb54ce902dc72cbe5d6
Dennis Wang <66754085+hellolol2016@users.noreply.github.com>
2022-02-14 21:00:17 -0800

    i changed one character :muscle:

--- pages/index.js
+++ pages/index.js
@@ -66,7 +66,7 @@ export default function Home({ router }) {
   return (
     <>
       <Head>
-        <meta charset="UTF-8" />
+        <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
         <title>WooTennis</title>
